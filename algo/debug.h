574e8b428a2315ee64ee62022fd6c2f7b6aac51a
zapwiz22 <kumar.jayant1916@gmail.com>
2025-11-22 02:10:10 +0530

    added the code for generating shuffled vectors in gen.cpp

--- algo/debug.h
+++ algo/debug.h
@@ -1,110 +1,293 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// template<typename A, typename B> ostream& operator<<(ostream& os, const
-// pair<A, B>& p) { return os << '(' << p.first << ", " << p.second << ')'; }
-
-// template<typename T_container, typename T = typename
-// enable_if<!is_same<T_container, string>::value, typename
-// T_container::value_type>::type> ostream& operator<<(ostream& os, const
-// T_container& v) { os << '{'; string sep; for (const T& x : v) os << sep << x,
-// sep = ", "; return os << '}'; }
-
-// void dbg_out() { cerr << endl; }
-// template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
-// cerr << ' ' << H; dbg_out(T...); }
-
-// #define dbg(...) cerr << "\033[1;31m[" << #__VA_ARGS__ << "] =\033[0m",
-// dbg_out(__VA_ARGS__)
-
-// #ifndef DEBUG_H
-// #define DEBUG_H
+#ifndef DEBUG_H
+#define DEBUG_H
 
 #include <bits/stdc++.h>
 using namespace std;
-// typedef long long ll;
-#define INF INT_MAX
-#define INFL LLONG_MAX
-#define nl "\n"
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-typedef pair<long, long> pll;
+// Forward declarations for all _print functions
+void _print(int x);
+void _print(long x);
+void _print(long long x);
+void _print(unsigned x);
+void _print(unsigned long x);
+void _print(unsigned long long x);
+void _print(float x);
+void _print(double x);
+void _print(long double x);
+void _print(char x);
+void _print(const char *x);
+void _print(const string &x);
+void _print(bool x);
 
-#define debug(x...)                                                            \
-  cerr << "[" << #x << "] = ";                                                 \
-  _print(x);                                                                   \
-  cerr << endl;
+template <typename T1, typename T2> void _print(const pair<T1, T2> &p);
 
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(long double t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(unsigned long long t) { cerr << t; }
+template <typename T> void _print(const vector<T> &v);
 
-template <class T, class V> void _print(pair<T, V> p);
-template <class T> void _print(vector<T> v);
-template <class T> void _print(set<T> v);
-template <class T, class V> void _print(map<T, V> v);
-template <class T> void _print(multiset<T> v);
-template <class T, class V> void _print(pair<T, V> p) {
-  cerr << "{";
-  _print(p.ff);
-  cerr << ",";
-  _print(p.ss);
-  cerr << "}";
-}
-template <class T> void _print(vector<T> v) {
-  cerr << "[ ";
-  for (T i : v) {
-    _print(i);
-    cerr << " ";
-  }
-  cerr << "]";
-}
-template <class T> void _print(set<T> v) {
-  cerr << "[ ";
-  for (T i : v) {
-    _print(i);
-    cerr << " ";
-  }
-  cerr << "]";
-}
-template <class T> void _print(unordered_set<T> v) {
-  cerr << "[ ";
-  for (T i : v) {
-    _print(i);
-    cerr << " ";
-  }
-  cerr << "]";
-}
-template <class T> void _print(multiset<T> v) {
-  cerr << "[ ";
-  for (T i : v) {
-    _print(i);
-    cerr << " ";
-  }
-  cerr << "]";
-}
-template <class T, class V> void _print(map<T, V> v) {
-  cerr << "[ ";
-  for (auto i : v) {
-    _print(i);
-    cerr << " ";
-  }
-  cerr << "]";
+template <typename T> void _print(const set<T> &s);
+
+template <typename T> void _print(const unordered_set<T> &s);
+
+template <typename T> void _print(const multiset<T> &s);
+
+template <typename K, typename V> void _print(const map<K, V> &m);
+
+template <typename K, typename V> void _print(const unordered_map<K, V> &m);
+
+template <typename T, size_t N> void _print(const array<T, N> &arr);
+
+template <typename T> void _print(const deque<T> &dq);
+
+template <typename T> void _print(stack<T> st);
+
+template <typename T> void _print(queue<T> q);
+
+template <typename T> void _print(priority_queue<T> pq);
+
+template <typename T> void _print(priority_queue<T, vector<T>, greater<T>> pq);
+
+template <typename T> void _print(const list<T> &lst);
+
+template <typename T> void _print(const forward_list<T> &flst);
+
+template <size_t N> void _print(const bitset<N> &bs);
+
+template <typename... Args> void _print(const tuple<Args...> &t);
+
+template <typename T> void _print(const complex<T> &c);
+
+// Core type printers implementation
+void _print(int x) { cerr << x; }
+void _print(long x) { cerr << x; }
+void _print(long long x) { cerr << x; }
+void _print(unsigned x) { cerr << x; }
+void _print(unsigned long x) { cerr << x; }
+void _print(unsigned long long x) { cerr << x; }
+void _print(float x) { cerr << x; }
+void _print(double x) { cerr << x; }
+void _print(long double x) { cerr << x; }
+void _print(char x) { cerr << '\'' << x << '\''; }
+void _print(const char *x) { cerr << '\"' << x << '\"'; }
+void _print(const string &x) { cerr << '\"' << x << '\"'; }
+void _print(bool x) { cerr << (x ? "true" : "false"); }
+
+// Pair
+template <typename T1, typename T2> void _print(const pair<T1, T2> &p) {
+    cerr << '{';
+    _print(p.first);
+    cerr << ',';
+    _print(p.second);
+    cerr << '}';
 }
 
-// #endif
+// Vector
+template <typename T> void _print(const vector<T> &v) {
+    cerr << '[';
+    for (size_t i = 0; i < v.size(); i++) {
+        if (i > 0)
+            cerr << ',';
+        _print(v[i]);
+    }
+    cerr << ']';
+}
+
+// Set
+template <typename T> void _print(const set<T> &s) {
+    cerr << '{';
+    for (auto it = s.begin(); it != s.end();) {
+        _print(*it);
+        if (++it != s.end())
+            cerr << ',';
+    }
+    cerr << '}';
+}
+
+// Unordered Set
+template <typename T> void _print(const unordered_set<T> &s) {
+    cerr << '{';
+    for (auto it = s.begin(); it != s.end();) {
+        _print(*it);
+        if (++it != s.end())
+            cerr << ',';
+    }
+    cerr << '}';
+}
+
+// Multiset
+template <typename T> void _print(const multiset<T> &s) {
+    cerr << '{';
+    for (auto it = s.begin(); it != s.end();) {
+        _print(*it);
+        if (++it != s.end())
+            cerr << ',';
+    }
+    cerr << '}';
+}
+
+// Map
+template <typename K, typename V> void _print(const map<K, V> &m) {
+    cerr << '{';
+    for (auto it = m.begin(); it != m.end();) {
+        _print(it->first);
+        cerr << ':';
+        _print(it->second);
+        if (++it != m.end())
+            cerr << ',';
+    }
+    cerr << '}';
+}
+
+// Unordered Map
+template <typename K, typename V> void _print(const unordered_map<K, V> &m) {
+    cerr << '{';
+    for (auto it = m.begin(); it != m.end();) {
+        _print(it->first);
+        cerr << ':';
+        _print(it->second);
+        if (++it != m.end())
+            cerr << ',';
+    }
+    cerr << '}';
+}
+
+// Array
+template <typename T, size_t N> void _print(const array<T, N> &arr) {
+    cerr << '[';
+    for (size_t i = 0; i < N; i++) {
+        if (i > 0)
+            cerr << ',';
+        _print(arr[i]);
+    }
+    cerr << ']';
+}
+
+// Deque
+template <typename T> void _print(const deque<T> &dq) {
+    cerr << '[';
+    for (size_t i = 0; i < dq.size(); i++) {
+        if (i > 0)
+            cerr << ',';
+        _print(dq[i]);
+    }
+    cerr << ']';
+}
+
+// Stack
+template <typename T> void _print(stack<T> st) {
+    vector<T> temp;
+    while (!st.empty()) {
+        temp.push_back(st.top());
+        st.pop();
+    }
+    reverse(temp.begin(), temp.end());
+    cerr << "stack:";
+    _print(temp);
+}
+
+// Queue
+template <typename T> void _print(queue<T> q) {
+    vector<T> temp;
+    while (!q.empty()) {
+        temp.push_back(q.front());
+        q.pop();
+    }
+    cerr << "queue:";
+    _print(temp);
+}
+
+// Priority Queue (max heap)
+template <typename T> void _print(priority_queue<T> pq) {
+    vector<T> temp;
+    while (!pq.empty()) {
+        temp.push_back(pq.top());
+        pq.pop();
+    }
+    cerr << "max_heap:";
+    _print(temp);
+}
+
+// Priority Queue (min heap)
+template <typename T> void _print(priority_queue<T, vector<T>, greater<T>> pq) {
+    vector<T> temp;
+    while (!pq.empty()) {
+        temp.push_back(pq.top());
+        pq.pop();
+    }
+    cerr << "min_heap:";
+    _print(temp);
+}
+
+// List
+template <typename T> void _print(const list<T> &lst) {
+    cerr << '[';
+    for (auto it = lst.begin(); it != lst.end();) {
+        _print(*it);
+        if (++it != lst.end())
+            cerr << ',';
+    }
+    cerr << ']';
+}
+
+// Forward List
+template <typename T> void _print(const forward_list<T> &flst) {
+    cerr << '[';
+    for (auto it = flst.begin(); it != flst.end();) {
+        _print(*it);
+        if (++it != flst.end())
+            cerr << ',';
+    }
+    cerr << ']';
+}
+
+// Bitset
+template <size_t N> void _print(const bitset<N> &bs) {
+    cerr << "bitset:\"";
+    for (size_t i = 0; i < N; i++) {
+        cerr << (bs.test(i) ? '1' : '0');
+    }
+    cerr << "\"";
+}
+
+// Tuple
+template <typename Tuple, size_t... Is>
+void _print_tuple(const Tuple &t, index_sequence<Is...>) {
+    cerr << '(';
+    (..., (cerr << (Is == 0 ? "" : ","), _print(get<Is>(t))));
+    cerr << ')';
+}
+
+template <typename... Args> void _print(const tuple<Args...> &t) {
+    _print_tuple(t, index_sequence_for<Args...>{});
+}
+
+// Complex
+template <typename T> void _print(const complex<T> &c) {
+    cerr << c.real();
+    if (c.imag() >= 0)
+        cerr << '+';
+    cerr << c.imag() << 'i';
+}
+
+// Main debug function
+#define debug(...) debug_print(#__VA_ARGS__, __VA_ARGS__)
+
+template <typename... Args>
+void debug_print(const char *names, Args &&...args) {
+    cerr << "\033[1;36m";
+    stringstream ss(names);
+    string name;
+    vector<string> vnames;
+
+    // Parse variable names
+    while (getline(ss, name, ',')) {
+        name.erase(0, name.find_first_not_of(" \t\n\r\f\v"));
+        name.erase(name.find_last_not_of(" \t\n\r\f\v") + 1);
+        vnames.push_back(name);
+    }
+
+    // Print each variable with its name
+    int i = 0;
+    ((cerr << (i++ ? " | " : "") << vnames[i - 1] << " = ", _print(args)), ...);
+    cerr << "\033[0m" << endl;
+}
+
+#endif // DEBUG_H
--- gen.cpp
+++ gen.cpp
@@ -9,10 +9,16 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int main() {
     int tests = 1;
     cout << tests << endl;
-    int n = uid(1, 10);
+    // int n = uid(1, 10);
+    int n = 10;
     cout << n << endl;
-    for (int i = 0; i < n; i++) {
-        cout << uid(1, 200) << " ";
+    vector<int> vec(n);
+    iota(vec.begin(), vec.end(), 1);
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(vec.begin(), vec.end(), g);
+    for (auto val : vec) {
+        cout << val << " ";
     }
     cout << endl;
 }
@@ -21,31 +27,11 @@ int main() {
   print uid(1,i-1) and i
 */
 
-// #include <bits/stdc++.h>
-// using namespace std;
-// #define ll long long
-// mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
-//
-// int random(int l, int r) {
-//     return l + RNG() % (r - l + 1);
-// }
-//
-// int main() {
-//     freopen("input.txt", "w", stdout); // Output test case to input.txt
-//     ll TESTS = 1;
-//     cout << TESTS << "\n";
-//
-//     while (TESTS--) {
-//         ll n = random(2, 5); // Random n in range [1, 1000]
-//         cout << n << "\n";
-//         ll m = random(2,n);
-//         cout << m << endl;
-//         // ll k = random(1,n);
-//         // cout << m << " " << k << endl;
-//         for (ll i = 0; i < m; i++) {
-//             cout << random(1,2*n) << " "; // Random array elements in [1,
-//             10^9]
-//         }
-//         cout << "\n";
-//     }
-// }
+/* how to randomly shuffle a vector
+   // Initialize random number generator
+    random_device rd;
+    mt19937 g(rd());
+
+    // Shuffle the vector
+    shuffle(v.begin(), v.end(), g);
+*/
