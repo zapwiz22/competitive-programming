b3afe365fbe172d1436d74e469e8df2f43d2678f
zapwiz22 <kumar.jayant1916@gmail.com>
2025-12-08 00:50:42 +0530

    some atcoder standard templates + Mint template Arpa blog

--- algo/jayant.cpp
+++ algo/jayant.cpp
@@ -1,43 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-
 template <typename T>
 using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-#ifdef LOCAL
-#include "debug.h"
-#else
-#define debug(...)
-#endif
-
-#define int long long 
-
-#define mod 1000000007
-#define mod1 998244353
-#define inf (long long)1e15
-#define linf (long long)2e18
-
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int mod = 1000000007, mod1 = 998244353, inf = 1e18;
 const int N = 1e3 + 1;
 
-void solve() {
+#define int long long
 
+void solve() {
+    
 }
 
-#undef int 
+#undef int
 
 int main() {
-	ios_base::sync_with_stdio(false), cin.tie(nullptr);
-	int TESTS = 1;
-	cin >> TESTS;
-
-	for (int Case = 1; Case <= TESTS; Case++) {
-		debug(Case);
-		solve();
-	}
-
-	return 0;
+    // #ifdef TNAYAJ
+    // freopen("int","r",stdin), freopen("out1","w",stdout), freopen("error","w",stderr);
+    // #endif
+    auto begin = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int tests = 1;
+    cin >> tests;
+    for (int i = 1; i <= tests; i++) {
+        cerr << "Case #" << i << ": " << endl;
+        solve();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n"; 
+    return 0;
 }
