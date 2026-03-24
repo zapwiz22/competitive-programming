53e7d77f992ff9532c3e894854cd4461ee868c84
zapwiz22 <kumar.jayant1916@gmail.com>
2026-03-24 15:30:06 +0530

    template

---
+++ templates/qt.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long  
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else 
+#define debug(...) 22 
+#endif 
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+
+    int tt = 1;
+    cin >> tt;
+    while (tt--) {
+        
+    }
+
+    return 0;
+}
---
+++ templates/solve.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+const int INF = 1e18, MOD = 1000000007; // 998244353;
+
+void solve() {
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+    int tt = 1;
+    cin >> tt;
+    for (int i = 1; i <= tt; i++) {
+        // cout << "Case #" << i << ": \n";
+        solve();
+    }
+
+    return 0;
+}
