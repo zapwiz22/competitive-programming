03b9b133c908a07135aa77cddd94998592ded9e5
zapwiz22 <kumar.jayant1916@gmail.com>
2026-03-27 04:09:50 +0530

    kmp and string hashing + heavy light decomposition

---
+++ graphs/heavy-light-decomposition.cpp
@@ -0,0 +1,60 @@
+template <class T, bool VALS_IN_EDGES> class HLD {
+  private:
+	int N, R, tim = 0;  // n, root node, time
+	vector<vector<int>> adj;
+	vector<int> par, siz, depth, rt, pos;  // parent, size, depth, root, position arrays
+	MaxSegmentTree<T> segtree;             // Modify as needed
+
+	/** Compute the size of each subtree and set parent-child relationship
+	 * Subtree of node v corresponds to segment [ pos[v], pos[v] + sz[v] ) */
+	void dfs_sz(int v) {
+		if (par[v] != -1) adj[v].erase(find(adj[v].begin(), adj[v].end(), par[v]));
+		for (int &u : adj[v]) {
+			par[u] = v, depth[u] = depth[v] + 1;
+			dfs_sz(u);
+			siz[v] += siz[u];
+			if (siz[u] > siz[adj[v][0]]) swap(u, adj[v][0]);
+		}
+	}
+
+	/** Assign positions for nodes
+	  * Path from v to the last vertex in ascending heavy path
+	    corresponds to [ pos[rt[v]], pos[v] ] */
+	void dfs_hld(int v) {
+		pos[v] = tim++;
+		for (int u : adj[v]) {
+			rt[u] = (u == adj[v][0] ? rt[v] : u);
+			dfs_hld(u);
+		}
+	}
+
+	/** process all heavy path and combine their results */
+	template <class B> void process(int u, int v, B op) {
+		for (; rt[u] != rt[v]; v = par[rt[v]]) {
+			if (depth[rt[u]] > depth[rt[v]]) swap(u, v);
+			op(pos[rt[v]], pos[v]);
+		}
+		if (depth[u] > depth[v]) swap(u, v);
+		op(pos[u] + VALS_IN_EDGES, pos[v]);
+	}
+
+  public:
+	HLD(vector<vector<int>> adj_, int _R)
+	    : N(adj_.size()), R(_R), adj(adj_), par(N, -1), siz(N, 1), depth(N), rt(N),
+	      pos(N), segtree(N)  // modify as needed
+	{
+		rt[R] = R;
+		dfs_sz(R);
+		dfs_hld(R);
+	}
+
+	T query_path(int u, int v) {
+		T res = 0;  // default value, modify depending on problem
+		process(u, v, [&](int l, int r) {
+			res = max(res, segtree.range_max(l, r + 1));  // modify depending on problem
+		});
+		return res;
+	}
+
+	void modify_node(int u, T val) { segtree.set(pos[u], val); }
+};
